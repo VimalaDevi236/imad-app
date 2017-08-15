8612d7a210825f497aa67277f87da634db9224d2
VimalaDevi236 <30768237+VimalaDevi236@users.noreply.github.com>
2017-08-15 20:21:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,50 +1,7 @@
-var express = require('express');
-var morgan = require('morgan');
-var path = require('path');
-
-var app = express();
-app.use(morgan('combined'));
-
-var articles ={
-    'article-one':{
-    title:'Article one vimala devi',
-    heading:'Article one',
-    date:'Sep 5 2016',
-    content:
-   ` <p>
-                this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.
-            </p>
-            <p>
-                this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.
-            </p>
-            <p>
-                this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.
-            </p>`
-},
-    'article-two':{ title:'Article two vimala devi',
-    heading:'Article two',
-    date:'Sep 10 2016',
-    content:
-   ` <p>
-                this is the content for my second article.
-     </p>`
-    } ,    
-    'article-three':{ title:'Article three vimala devi',
-    heading:'Article three',
-    date:'Sep 15 2016',
-    content:
-   ` <p>
-                this is the content for my third article.
-     </p>`}
-};
-function createTemplate(data){
-    var title=data.title;
-    var date=data.date;
-    var content=data.content;
-var htmlTemplate=`<html>
+<html>
     <head>
         <title>
-            ${title}
+            Article one Vimala Devi
         </title>
         <meta name='viewport' content='width=device-width,initial-scale=1'/>
         <link href="/ui/style.css" rel="stylesheet" />
@@ -57,48 +14,22 @@ var htmlTemplate=`<html>
         </div>
         <hr>
         <h1>
-            ${heading}
+            Article One
         </h1>
         <div>
-            ${date}
+            Sep 5,2016
         </div>
         <div>
-           ${content}
+            <p>
+                this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.
+            </p>
+            <p>
+                this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.
+            </p>
+            <p>
+                this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.
+            </p>
         </div>
       </div>
     </body>
 </html>
-;`
-return htmlTemplate;
-}
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-
-app.get('/:articleName',function(req,res){
-    //article-name=article-one
-    //articles[articleName]={}content object for article-one
-    var articleName = req.params.articleName;
- res.send(createTemplate(articles[articleName]));
-});
-
-
-
-
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-
-// Do not change port, otherwise your app won't run on IMAD servers
-// Use 8080 only for local development if you already have apache running on 80
-
-var port = 8080;
-app.listen(port, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
-});
