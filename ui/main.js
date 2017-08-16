ba2c27af977d6a15f152d555cc8b265151811f54
VimalaDevi236 <30768237+VimalaDevi236@users.noreply.github.com>
2017-08-16 22:03:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,6 +7,15 @@ element.innerHTML='new value';
 
 //move the image
 var img=document.getElementById('madi');
+var marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft+"px";
+}
 img.onclick =function(){
-    img.style.marginLeft="100 px";
+  
+   
+    var interval=setInterval(moveLeft,100); 
+    
+    
 };
