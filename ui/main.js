a9c3733395b434dce0c507ee50cc20972a72fd95
VimalaDevi236 <30768237+VimalaDevi236@users.noreply.github.com>
2017-08-20 12:32:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,9 +2,39 @@
 var button=document.getElementById('counter');
 
 
-var submit=document.getElementById("submit_btn");
+
 button.onclick = function(){
-    //create a reuest
+    //create a request
+    var request = new XMLHttpRequest();
+    //capture the response and store it in a variable
+    request.onreadystatechange = function(){
+        if (request.readystate===XMLHttpRequest.DONE){
+            //TAKE SOME ACTION
+            if (request.status===200){
+                 //captures the names and render it as a list
+             var counter=request.responseText;
+             var span=document.getElementById('count');
+             span.innerHTNL=counter.toString();
+            
+            }//inner if
+            }//outer if
+        
+        //NOT DONE YET
+        
+    };//request function
+    
+  
+    
+    request.open('GET','http://svimaladevisris.imad.hasura-app.io/counter',true);//required
+    request.send(null);//required
+};//button click function
+
+//submit name
+var nameInput= document.getElementById("name");
+var name = nameInput.value;
+var submit=document.getElementById("submit_btn");
+submit.onclick = function(){
+    //create a request
     var request = new XMLHttpRequest();
     //capture the response and store it in a variable
     request.onreadystatechange = function(){
@@ -21,19 +51,19 @@ button.onclick = function(){
               var ul=document.getElementById('namelist');
               ul.innerHTML=list;
     
-            }
-            }
+            }//inner if
+            }//outer if
         
         //NOT DONE YET
         
-    };
+    };//request function
     
-    var nameInput= document.getElementById("name");
-    var name = nameInput.value;
-    //make the request
+  
     request.open('GET','http://svimaladevisris.imad.hasura-app.io/submit-name?name='+name,true);
+    
     request.send(null);
-};
+};//submit click function
+
 
 
     
