d7ca94de9a69fa30fd01ffd2de234b51f833fbdf
VimalaDevi236 <30768237+VimalaDevi236@users.noreply.github.com>
2017-08-20 21:53:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -116,7 +116,7 @@ app.post('/login',function(req,res){
           }
           else{
           var dBString=result.rows[0].password;
-          dBString.split('$')[2];
+           var salt= dBString.split('$')[2];
           var hashedPassword=hash(password,salt);
           if(hashedPassword === dBString){
           res.send('credentials correct!');
