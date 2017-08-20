50396032a34d873841e95210383de0122f637726
VimalaDevi236 <30768237+VimalaDevi236@users.noreply.github.com>
2017-08-20 12:12:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,9 @@
 //counter code
 var button=document.getElementById('counter');
 
-
+var nameInput= document.getElementById("name");
+var name = nameInput.value;
+var submit=document.getElementById("submit_btn");
 button.onclick = function(){
     //create a reuest
     var request = new XMLHttpRequest();
@@ -10,10 +12,16 @@ button.onclick = function(){
         if (request.readystate===XMLHttpRequest.DONE){
             //TAKE SOME ACTION
             if (request.status===200){
-            var counter=request.responseText;
-              var span=document.getElementById('count');
-              span.innerHTML=counter.toString();    
-                
+                 //captures the names and render it as a list
+             var names=request.responseText;
+             names=JSON.parse(names);
+              var list="";
+              for(var i=0;i<names.length;i++){
+               list += "<li>"+names[i]+"</li>";   
+              }
+              var ul=document.getElementById('namelist');
+              ul.innerHTML=list;
+    
             }
             }
         
@@ -21,24 +29,9 @@ button.onclick = function(){
         
     };
     //make the request
-    request.open('GET','http://svimaladevisris.imad.hasura-app.io/counter',true);
+    request.open('GET','http://svimaladevisris.imad.hasura-app.io/submit-name?name='+name,true);
     request.send(null);
 };
 
-var nameInput= document.getElementById("name");
-var name = nameInput.value;
-var submit=document.getElementById("submit_btn");
-submit.onclick=function(){
-    //make a reuest to the server and send the name
-    //captures the names and render it as a list
+
     
-  var names=['name1','name2','name3','name4'];
-  var list="";
-  for(var i=0;i<names.length;i++){
-   list += "<li>"+names[i]+"</li>";   
-  }
-  var ul=document.getElementById('namelist');
-  ul.innerHTML=list;
-    
-    
-};
