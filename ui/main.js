1f4fa4a344c49e0748f04201adbdbb88a0e3f7a9
VimalaDevi236 <30768237+VimalaDevi236@users.noreply.github.com>
2017-08-26 10:34:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,32 +1,6 @@
-//counter code
-var button=document.getElementById('counter');
 
 
-
-button.onclick = function(){
-    //create a request
-    var request = new XMLHttpRequest();
-    //capture the response and store it in a variable
-    request.onreadystatechange = function(){
-        if (request.readyState===XMLHttpRequest.DONE){
-            //TAKE SOME ACTION
-            if (request.status===200){
-                 //captures the names and render it as a list
-             var counter=request.responseText;
-             var span=document.getElementById('count');
-             span.innerHTML=counter.toString();
-            
-            }//inner if
-            }//outer if
-        
-        //NOT DONE YET
-        
-    };//request function
-    request.open('GET','http://svimaladevisris.imad.hasura-app.io/counter',true);//required
-    request.send(null);//required
-};//button click function
-
-//submit name
+//submit username/password to login
 var submit=document.getElementById('submit_btn');
 submit.onclick = function(){
     //create a request
@@ -37,26 +11,27 @@ submit.onclick = function(){
             //TAKE SOME ACTION
             if (request.status===200){
                  //captures the names and render it as a list
-             var names=request.responseText;
-             names=JSON.parse(names);
-              var list='';
-              for(var i=0;i<names.length;i++){
-               list += "<li>"+names[i]+"</li>";   
-              }
-              var ul=document.getElementById('namelist');
-              ul.innerHTML=list;
-    
-            }//inner if
-            }//outer if
-        
+                console.log('user logged in');
+                alert('logged in successfully');
+                }else if(request.status === 403){
+                    alert('username/password is invalid');
+                }else if(request.status===500){
+                    alert('something went wrong ...');
+                }
+            
+        }
         //NOT DONE YET
         
     };//request function
     
-    var nameInput= document.getElementById("name");
+    var username= document.getElementById("username").value;
+    var password= document.getElementById("password").value;
     var name = nameInput.value;
-    request.open('GET','http://svimaladevisris.imad.hasura-app.io/submit-name?name='+name,true);
-    request.send(null);
+    console.log(username);
+    console.log(password);
+    request.open('POST','http://svimaladevisris.imad.hasura-app.io/login',true);
+    request.setRequestHeader('Content-Type','application/json');
+    request.send(JSON.stringify({username:username,password:password}));
 };//submit click function
 
 
